6938a879bed488bbf7c73bff1eb884aff46c093c
José Catagua <joesitop@gmail.com>
2018-07-12 18:47:18 -0500

    Update wp-config.php
    new

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define('DB_PASSWORD', 'admin');
 define('DB_HOST', 'ramsar2018dongoyo.000webhostapp.com');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
